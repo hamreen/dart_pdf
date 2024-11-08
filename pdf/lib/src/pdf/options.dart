4e292af6fa3977b99c5bba5b52abdc1480fa41b0
hamreen <157309095+hamreen@users.noreply.github.com>
2024-11-08 21:58:54 +0300

    Update options.dart

--- pdf/lib/src/pdf/options.dart
+++ pdf/lib/src/pdf/options.dart
@@ -1,5 +1,5 @@
 /// Whether to use the Bidi algorithm to detect RTL text.
-const bool useBidi = bool.fromEnvironment('use_bidi', defaultValue: true);
+const bool useBidi = bool.fromEnvironment('use_bidi', defaultValue: false);
 
 /// Whether to use the Arabic algorithm.
 const bool useArabic =
